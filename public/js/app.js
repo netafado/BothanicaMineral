1262a3761f5af00a4f841c871fdf32f37c8efee4
Isaias F. dos Santos <isaias.fran@gmail.com>
2018-02-28 09:48:38 -0300

    alter index

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+"use strict";
+(function($){
+
+
+    //esse evento acontece quando toda a página é carregada
+    window.addEventListener('load', function(){
+        $('#loader').fadeOut(200);
+    })
+})($)
