e98508e3e9eb4d14810608d625344f3a0ab096e7
Isaias F. dos Santos <isaias.fran@gmail.com>
2018-03-05 10:22:17 -0300

    melhoras menu

--- public/js/app.js
+++ public/js/app.js
@@ -29,10 +29,7 @@
             
 
         }
-
         desenhar();
-
-
     }
 
 
@@ -49,7 +46,8 @@
       
 
 
-    $('a.goesTo, .link-footer').bind('click', function (event) {
+    $('.goesTo, .link-footer').on('click', function (event) {
+        console.log(this);
         var $anchor = $(this);
 
         $('html, body').stop().animate({ scrollTop: $($anchor.attr('href')).offset().top }, 1000, 'swing');
@@ -58,6 +56,40 @@
         // linear, swing, jswing, easeInQuad, easeInCubic, easeInQuart, easeInQuint, easeInSine, easeInExpo, easeInCirc, easeInElastic, easeInBack, easeInBounce, easeOutQuad, easeOutCubic, easeOutQuart, easeOutQuint, easeOutSine, easeOutExpo, easeOutCirc, easeOutElastic, easeOutBack, easeOutBounce, easeInOutQuad, easeInOutCubic, easeInOutQuart, easeInOutQuint, easeInOutSine, easeInOutExpo, easeInOutCirc, easeInOutElastic, easeInOutBack, easeInOutBounce
 
     });
+
+    function mostrarIrTopo(e){
+
+        let btn = $('#ir-topo');
+        let posY = e.currentTarget.scrollY
+        if(posY > 800){
+            $(btn).fadeIn(200);
+        }
+        else{
+            $(btn).fadeOut(200);
+        }
+    }
+
+    function menuLeort(e){
+        var header = $('#topo');
+        let posY = e.currentTarget.scrollY
+        if(posY > 400){
+            $(header).addClass('pos-fixed');
+        }
+        else{
+            $(header).removeClass('pos-fixed');
+        }
+    }
+
+    window.addEventListener('scroll', function(e){
+        menuLeort(e);
+        mostrarIrTopo(e);
+    });
+
+    $('.link-topo').on('click', function(){
+        $('.link-topo').removeClass('ativo');
+        $(this).addClass('ativo');
+    })
+
     //esse evento acontece quando toda a página é carregada
     window.addEventListener('load', function(){
         $('#loader').fadeOut(200);
