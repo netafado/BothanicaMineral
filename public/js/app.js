976c37b8951e5631e64a70e4fd7aff7286d039ae
Liliane <liliane@happybiz.com.br>
2018-03-14 18:06:38 -0300

    alteração do Logo

--- public/js/app.js
+++ public/js/app.js
@@ -61,7 +61,37 @@
         asNavFor: '#bannerDep',
         dots: true,
         centerMode: true,
-        focusOnSelect: true
+        focusOnSelect: true,
+        nextArrow: '<img src="img/seta-cat-right.png" class="seta-right" />"',
+        prevArrow: '<img src="img/seta-cat-left.png" class="seta-left" />"', 
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]       
     });
 
 
